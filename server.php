eead8a6c4ea4ff9e5ae6513e946b291db3e57033
Omar-Codess <omartaki8@gmail.com>
2022-11-30 08:58:29 +0100

    aggiunta chiamata axios in created

--- server.php
+++ server.php
@@ -2,3 +2,12 @@
 
 $string = file_get_contents("todo.json");
 $todos = json_decode($string, true);
+
+if (isset($_POST["newTodo"])) {
+    $new_todo = $_POST["newTodo"];
+    $todos[] = $new_todo;
+    file_put_contents("todo.json", json_encode($todos));
+}
+
+header("Content-Type: application/json");
+echo json_encode($todos);
